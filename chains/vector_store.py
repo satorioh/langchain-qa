34034cfdbb79a1f4e196cf8dff0315c777972eff
Robin.Wang <3Jg$94sbQ@q1>
2023-05-08 17:57:42 +0800

    feat(llm prompt): prompt finish

--- chains/vector_store.py
+++ chains/vector_store.py
@@ -1,4 +1,6 @@
 from langchain.vectorstores import FAISS
+from configs.global_config import *
+from typing import List
 
 
 class VectorStore:
@@ -13,9 +15,24 @@ class VectorStore:
             self.db.save_local("faiss_index")
         print("init vector store finished")
 
-    def similarity_search(self, query):
+    @staticmethod
+    def generate_prompt(related_docs: List,
+                        query: str,
+                        prompt_template=PROMPT_TEMPLATE) -> str:
+        context = "\n".join([doc.page_content for doc in related_docs])
+        prompt = prompt_template.replace("{question}", query).replace("{context}", context)
+        return prompt
+
+    def get_similarity_docs(self, query):
         print("start similarity search...")
         result = self.db.similarity_search(query)
         print("similarity search result:")
         print(result)
         return result
+
+    def get_vector_prompt(self, query):
+        # TODO query need vector
+        related_docs = self.get_similarity_docs(query)
+        prompt = self.generate_prompt(related_docs, query)
+        print(prompt)
+        return prompt
--- main.py
+++ main.py
@@ -11,16 +11,14 @@ if __name__ == "__main__":
     embeddings = CustomEmbeddings().embeddings
     split_docs = CustomTextSplitter(test_folder).get_split_docs()
     vector_store = VectorStore(embeddings, split_docs)
-    vector_store.similarity_search("宅基地使用权及房屋所有权转移登记")
-    # llm = ChatGLM()
-    # while True:
-    #     human_input = input("Human: ")
-    #
-    #     begin_time = time.time() * 1000
-    #     # 请求模型
-    #     # TODO: llm stop key word
-    #     response = llm(human_input, stop=["stop"])
-    #     end_time = time.time() * 1000
-    #     used_time = round(end_time - begin_time, 3)
-    #     print(f"chatGLM process time: {used_time}ms")
-    #     print(f"ChatGLM: {response}")
+    human_input = vector_store.get_vector_prompt("爸爸和妈妈可以结婚吗？")
+    llm = ChatGLM()
+
+    begin_time = time.time() * 1000
+    # 请求模型
+    # TODO: llm stop key word
+    response = llm(human_input, stop=["stop"])
+    end_time = time.time() * 1000
+    used_time = round(end_time - begin_time, 3)
+    print(f"chatGLM process time: {used_time}ms")
+    print(f"ChatGLM: {response}")
